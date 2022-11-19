5448d2340900a07db9cbf864b9ddf927979cf675
Maciej Opaliński <poprostumieciek@gmail.com>
2022-11-19 23:40:55 +0100

    stuff

--- pages/MyCompostersPage.js
+++ pages/MyCompostersPage.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import { Portal, FAB, List, Divider } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import base from '../styles/base';
 
 import { Composter } from '../components';
 
 export const MyCompostersPage = () => {
+    const [composters, setComposters] = useState(null);
+
+    useEffect(() => {
+        AsyncStorage.getItem('composters', (err, v) => {
+            if (err) return;
+            setComposters(JSON.parse(v));
+        });
+    }, []);
+
     return (
         <View style={{ padding: 20 }}>
             <Text
@@ -15,18 +25,18 @@ export const MyCompostersPage = () => {
                 My Composters
             </Text>
 
-            <ScrollView style={{ marginTop: 20 }}>
-                {Array.from({ length: 10 }).map((_, idx) => (
-                    <Composter
-                        data={{
-                            id: `composter-${idx + 1}`,
-                            name: `Composter ${idx + 1}`,
-                        }}
-                    />
-                ))}
-            </ScrollView>
-
-            <Divider style={{ width: '100%' }} />
+            {composters && (
+                <ScrollView style={{ marginTop: 20 }}>
+                    {Array.from({ length: 10 }).map((_, idx) => (
+                        <Composter
+                            data={{
+                                id: `composter-${idx + 1}`,
+                                name: `Composter ${idx + 1}`,
+                            }}
+                        />
+                    ))}
+                </ScrollView>
+            )}
 
             {/* <FAB.Group icon='plus' actions={[]} onStateChange={() => {}} /> */}
         </View>
