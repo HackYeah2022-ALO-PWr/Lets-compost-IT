81e23ca5433ccfe29db23efa696ff75ff53e18f8
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-11-20 01:00:59 +0100

    qwtuioiytrew

--- components/Composter.js
+++ components/Composter.js
@@ -1,17 +1,66 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { List } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Composter = ({ data }) => {
+    const [composter, setComposter] = useState(null);
+    const [events, setEvents] = useState(null);
+
+    AsyncStorage.getItem('composters', (err, v) => {
+        if (err) return;
+        arr = JSON.parse(v);
+        arr.forEach(comp => {
+            if(comp.id === data.id) {
+                setComposters(comp);
+            }
+        });
+    });
+
+    AsyncStorage.getItem('events', (err, v) => {
+        if (err) return;
+        arr = JSON.parse(v);
+        setEvents(arr.filter(event => event.composterID === composter.id));
+    });
+
+    let avLvll = 0;
+    let filled = 0;
+    
+    events.forEach((event, i) => {
+        const d = new Date(event.date * 1000);
+        const now = new Date();
+        const diff = d - now;
+        const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
+        const lvl = Math.round(diffDays / 100);
+        avLvll += lvl * event.amount;
+        filled += event.amount;
+    });
+
+    const lvl = avLvll / filled;
+    filled /= composter.volume;
+    let name = '';
+    if(lvl >= 100) name = 'b';
+    else if(lvl >= 50) name = 'y';
+    else if(lvl >= 0) name = 'g';
+
+    if(filled > 80) name += '5';
+    else if(filled > 60) name += '4';
+    else if(filled > 40) name += '3';
+    else if(filled > 20) name += '2';
+    else if(filled > 0) name += '1';
+    else if(filled === 0) name = 'empty';
+    name += '.jpg';
     return (
         <List.Accordion
-            title={data.name}
+            title={composter.name}
             left={(props) => <List.Icon {...props} icon='folder' />}
-            id={data.id}
+            id={composter.id}
             style={{ padding: 10 }}
         >
             <View style={{ padding: 10 }}>
-                <Text>{data.name}</Text>
+                <Text>{composter.name}</Text>
+                <Text>{name}</Text>
+                {/* <Image source={require(`../assets/${name}`)} /> */}
             </View>
         </List.Accordion>
     );
